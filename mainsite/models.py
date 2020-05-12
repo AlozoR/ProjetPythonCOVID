43aae8b1ab5c3ae19c6375191239987b09bc55f2
AlozoR <nicolas-mery@neuf.fr>
2020-05-13 00:05:09 +0200

    début création formulaire inscription

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Foyer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    adresse = models.TextField(max_length=100)
+    ville = models.TextField(max_length=50)
+    est_une_residence = models.BooleanField(default=True)
+    habitants = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f'Foyer de {self.user.first_name} {self.user.last_name}'
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
-# Create your views here.
+from .models import Foyer
+
+class DetailView(generic.DetailView):
+    model = Foyer
+    template_name = 'mainsite/sign_in.html'
